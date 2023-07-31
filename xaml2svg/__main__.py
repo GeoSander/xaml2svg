67e9dc97a0af2bb093dde9d420bd895d4506a37c
Sander Schaminee <sander.schaminee@geocat.net>
2023-07-31 17:14:41 +0200

    Fixed color processing (fill + opacity)

--- xaml2svg/__main__.py
+++ xaml2svg/__main__.py
@@ -53,6 +53,9 @@ ELEM_LINEAR_GRADIENT_BRUSH = f'{{{NS_ROOT}}}LinearGradientBrush'
 ELEM_SOLID_COLOR_BRUSH = f'{{{NS_ROOT}}}SolidColorBrush'
 ELEM_GRADIENT_STOP = f'{{{NS_ROOT}}}GradientStop'
 
+NO_COLOR = 'none'
+FULL_OPACITY = '1'
+
 
 def scale(value, value_max, scaled_max):
     """ Scales a value using the same scale factor required to scale from value_max to scaled_max.
@@ -112,42 +115,42 @@ def svg_group(parent: ETree.Element, tf_elem: Optional[ETree.Element]):
 #     # spread = brush.attrib.get(ATTR_SPREAD_METHOD)  TODO (defaults to 'pad')
 
 
-def argb_to_rgba(color: str):
-    """ Converts WPF ARGB colors to SVG RGBA. Leaves non-hexadecimal color types unchanged. """
-    if not color.startswith('#'):
-        return color
-    return f'#{color[3:]}{color[1:3]}'
+def split_argb(color: str) -> tuple[str, str]:
+    """ Converts WPF ARGB colors to SVG RGB and an opacity value.
+    Leaves non-hexadecimal color types unchanged. The default opacity value is 1. """
+    if not color.startswith('#') or (color.startswith('#') and len(color) == 7):
+        # Named color or already RGB hex
+        if color.lower() == 'transparent':
+            color = NO_COLOR
+        return color.lower(), FULL_OPACITY
+
+    # Convert hex opacity to decimal
+    opacity = scale(int.from_bytes(bytes.fromhex(color[1:3]), 'little'), 255, 1)
+    return f'#{color[3:].lower()}', str(round(opacity, 2))
 
 
-def process_brush(brush: ETree.Element, defs: ETree.Element) -> str:
-    """ Processes a GeometryDrawing.Brush element to turn it into an SVG fill. """
-    result = 'none'
+def process_brush(brush: ETree.Element, defs: ETree.Element) -> tuple[str, str]:
+    """ Processes a GeometryDrawing.Brush element to turn it into an SVG fill and fill-opacity. """
 
     try:
         # Actual brush definition is first child
         brush = brush[0]
     except IndexError:
         print(f"Empty GeometryDrawing.Brush element encountered")
-        return result
+        return NO_COLOR, FULL_OPACITY
 
     if brush.tag == ELEM_SOLID_COLOR_BRUSH:
         # Process solid color brush
         color = brush.attrib.get(ATTR_COLOR, '')
         if not color or color.lower() == 'transparent' or not color.startswith('#'):
             # No supported color or transparent: output 'none'
-            return result
-        opacity = brush.attrib.get(ATTR_OPACITY)
-        if opacity:
-            # Adjust color to become RGBa
-            opacity = hex(int(scale(float(opacity), 1, 255))).upper().removeprefix('0X')
-            if len(color) == 9:
-                result = f'#{color[3:]}{opacity}'
-            elif len(color) == 7:
-                result = color + opacity
+            return NO_COLOR, FULL_OPACITY
+        opacity = brush.attrib.get(ATTR_OPACITY, FULL_OPACITY)
 
     else:
         print('Gradient brushes are not supported yet: setting color to "red"')
-        result = 'red'
+        color = 'red'
+        opacity = FULL_OPACITY
         # # Process gradient brush TODO
         # gradient = None
         # if brush.tag == ELEM_RADIAL_GRADIENT_BRUSH:
@@ -158,22 +161,21 @@ def process_brush(brush: ETree.Element, defs: ETree.Element) -> str:
         #     defs.append(gradient)
         #     result = f"url('#{gradient.attrib['id']}')"
 
-    return result
+    return color, opacity
 
 
 def svg_path(parent: ETree.Element, path_elem: ETree.Element, defs: ETree.Element):
     """ Turns a GeometryDrawing into an SVG path, ellipsis, or rect. """
     fill = path_elem.attrib.get(ATTR_BRUSH, path_elem.find(ELEM_GEOMETRY_DRAWING_BRUSH))
     if isinstance(fill, str):
-        fill = argb_to_rgba(fill)
-        if fill.lower() == 'transparent':
-            fill = 'none'
+        fill, opacity = split_argb(fill)
     else:
-        fill = process_brush(fill, defs)
+        fill, opacity = process_brush(fill, defs)
 
+    sub_elem = None
     geometry = path_elem.attrib.get(ATTR_GEOMETRY, path_elem.find(ELEM_GEOMETRY_DRAWING_GEOMETRY))
     if isinstance(geometry, str):
-        ETree.SubElement(parent, 'path', d=geometry.removeprefix('F1 '), fill=fill)
+        sub_elem = ETree.SubElement(parent, 'path', d=geometry.removeprefix('F1 '), fill=fill)
     else:
         # First child is actual geometry object
         geometry = geometry[0]
@@ -185,17 +187,21 @@ def svg_path(parent: ETree.Element, path_elem: ETree.Element, defs: ETree.Elemen
             cx, cy = geometry.attrib.get(ATTR_CENTER, '0,0').split(',')
             radius_x = geometry.attrib.get(ATTR_RADIUS_X, '1')
             radius_y = geometry.attrib.get(ATTR_RADIUS_Y, '1')
-            ETree.SubElement(parent, 'ellipse', cx=cx, cy=cy, rx=radius_x, ry=radius_y, fill=fill)
+            sub_elem = ETree.SubElement(parent, 'ellipse', cx=cx, cy=cy, rx=radius_x, ry=radius_y, fill=fill)
         elif geometry.tag == ELEM_RECTANGLE:
             # <rect x="120" y="5" width="100" height="100" />
             x, y, width, height = geometry.attrib.get(ATTR_RECT, '0,0,1,1')
-            rectangle = ETree.SubElement(parent, 'rect', width=width, height=height, fill=fill)
+            sub_elem = rectangle = ETree.SubElement(parent, 'rect', width=width, height=height, fill=fill)
             if x > 0:
                 rectangle.attrib['x'] = x
             if y > 0:
                 rectangle.attrib['y'] = y
         else:
             print(f'Encountered an unsupported geometry of type {geometry.tag}')
+
+    # Set fill opacity if needed
+    if sub_elem is not None and float(opacity) < 1:
+        sub_elem.attrib['fill-opacity'] = opacity
 
 
 def image_size(k: str) -> Optional[int]:
